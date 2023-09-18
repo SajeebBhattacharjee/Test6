ce6ed17d8408629bdf0d25fed40959b01404a9ed
SajeebBhattacharjee <sajeebshovo682gmail.com>
2023-09-18 21:00:28 +0600

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shopping List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Map<String, dynamic>> shoppingItems = [
+    {'name': 'Apples', 'icon': Icons.shopping_cart},
+    {'name': 'Bananas', 'icon': Icons.shopping_cart},
+    {'name': 'Bread', 'icon': Icons.shopping_cart},
+    {'name': 'Milk', 'icon': Icons.shopping_cart},
+    {'name': 'Eggs', 'icon': Icons.shopping_cart},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shopping List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: shoppingItems.length,
+        itemBuilder: (context, index) {
+          final item = shoppingItems[index];
+          return ListTile(
+            leading: Icon(item['icon']),
+            title: Text(item['name']),
+            onTap: () {
+
+            },
+          );
+        },
+      ),
+    );
+  }
+}
